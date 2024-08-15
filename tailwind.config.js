556208839d1d990554b90a036e9bd38543424fe8
antidoid <singhpiyush998@gmail.com>
2024-08-15 12:06:19 +0530

    forgot this

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        pink: {
+          450: "#FF4683",
+        },
+        blue: {
+          450: "#105BE4",
+        },
+        yellow: {
+          450: "#FFCC02",
+        },
+      },
+      fontFamily: {
+        Bounce: ["Birthstone Bounce", "cursive"],
+        Inter: ["Inter", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
