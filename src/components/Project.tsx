3abff0f90201dce7444898ed83d184fb7c0faffe
antidoid <singhpiyush998@gmail.com>
2024-08-17 22:10:11 +0530

    Update: project section to show shapes

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -3,29 +3,69 @@ type ProjectProps = {
   description: string;
   projectSrc: string;
   imageSrc: string;
+  shape: "square" | "circle" | "triangle";
+  direction: "normal" | "reverse";
 };
 
-// enum Shape {
-//   "square",
-//   "circle",
-//   "polygon",
-// }
+function Project({
+  name,
+  description,
+  projectSrc,
+  imageSrc,
+  shape,
+  direction,
+}: ProjectProps) {
+  const shapeEl = (() => {
+    switch (shape) {
+      case "circle":
+        return (
+          <div
+            className={`size-10 bg-blue-450 rounded-full absolute ${direction === "normal" ? "-left-14" : "-right-14"
+              }`}
+          ></div>
+        );
+      case "triangle":
+        return (
+          <div
+            className={`w-0 h-0 border-l-[16px] border-r-[16px] border-b-[32px] border-l-transparent border-r-transparent border-b-pink-450 absolute ${direction === "normal" ? "-left-14" : "-right-14"
+              }`}
+          ></div>
+        );
+      case "square":
+        return (
+          <div
+            className={`size-10 bg-yellow-450 absolute ${direction === "normal" ? "-left-14" : "-right-14"
+              }`}
+          ></div>
+        );
+    }
+  })();
 
-function Project({ name, description, projectSrc, imageSrc }: ProjectProps) {
   return (
     <div className="p-[1px] bg-gradient-to-r from-blue-450 via-pink-450 to-yellow-450 rounded-3xl">
       <div className="bg-black grid grid-cols-2 rounded-3xl py-10 px-10">
-        <div className="font-Inter flex flex-col gap-10 pt-9 px-8">
-          <h2 className="font-normal text-4xl">{name}</h2>
-          <p className="font-extralight text-2xl w-4/5">{description}</p>
-          <a href={projectSrc} className="font-Bounce text-2xl">
-            View Project
-            <span className="bg-gradient-to-r from-blue-450 via-pink-450 to-yellow-450 text-transparent bg-clip-text">
-              {" - - - >"}
-            </span>
-          </a>
+        <div
+          className={`flex pt-9 ${direction == "reverse" ? "order-2" : "order-1"}`}
+        >
+          <div
+            className={`w-fit font-Inter flex flex-col gap-10 ${direction == "reverse" ? "items-end pr-8" : "items-start pl-8 "}`}
+          >
+            <div className="w-fit relative">
+              {shapeEl}
+              <h2 className="font-normal text-4xl">{name}</h2>
+            </div>
+            <p className="font-extralight text-2xl w-4/5">{description}</p>
+            <a href={projectSrc} className="font-Bounce text-2xl">
+              View Project
+              <span className="bg-gradient-to-r from-blue-450 via-pink-450 to-yellow-450 text-transparent bg-clip-text">
+                {" - - - >"}
+              </span>
+            </a>
+          </div>
         </div>
-        <div className="h-96 w-auto">
+        <div
+          className={`h-96 w-auto ${direction == "reverse" ? "order-1" : "order-2"}`}
+        >
           <img className="rounded-3xl h-full w-full" src={imageSrc} />
         </div>
       </div>
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -17,18 +17,24 @@ function Projects() {
         description="Make mealtime magical again! Share your creations, connect and meet like-minded cooks, and turn every dinner into a delightful social experience."
         projectSrc="https://www.figma.com/design/SXZuA3NfHpXpyulrhqGqdO/cookbook-2.0-(Community)?node-id=0-1&t=H5i7tVlUdkwRWCg6-1"
         imageSrc={CookbooksImg}
+        direction="normal"
+        shape="circle"
       />
       <Project
         name="REVA HACK </> 2023"
         description="A user-friendly and engaging website and mobile app for RevaHack</> 2023, the largest tech event at Reva University. The goal was to create a digital experience that would attract potential participants, sponsors, and judges, while also providing them with all the necessary information about the event."
         projectSrc="https://www.figma.com/design/8PXsz1XaM0k0eCfY8V2iKO/revahack-ui?node-id=0-1&t=rpNWmuCqRjgbsvmu-1"
         imageSrc={RH}
+        direction="reverse"
+        shape="triangle"
       />
       <Project
         name="STOCKSTREET"
         description="A user-friendly website design to check stocks and learn about stock market."
         projectSrc="https://www.figma.com/design/NCY1Oti3jTJDMLMjwJnD9W/stockstreet-design?node-id=51-1732&t=6bEeerA2t5VHGBCy-1"
         imageSrc={StockStreet}
+        direction="normal"
+        shape="square"
       />
     </div>
   );
